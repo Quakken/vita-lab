06753aed4b82a50e4eb202a1045c632192bb093f
Quinn Farrell <quinnmfarrell@hotmail.com>
2026-07-08 19:18:46 -0700

    Basic hello world example

--- hello_world/main.cpp
+++ hello_world/main.cpp
@@ -1,5 +1,29 @@
 #include <psp2/kernel/processmgr.h>
 
-int main() {
+#include "debugScreen.h"
 
+#define printf psvDebugScreenPrintf
+
+int main() {
+	// Scale font to make it easier to read
+	auto* font = psvDebugScreenScaleFont2x(psvDebugScreenGetFont());
+	psvDebugScreenSetFont(font);
+
+	// Initialize debug screen
+	psvDebugScreenInit();
+
+	// Print a message
+	printf("Vita hello world test\n");
+	sceKernelDelayThread(3 * 1'000'000);
+
+	// Count down to exit
+	printf("Exiting in...\n");
+	for (auto i = 3; i > 0; --i) {
+		printf("%i\n", i);	
+		sceKernelDelayThread(1'000'000);
+	}
+
+	psvDebugScreenFinish();
+
+	return 0;
 }
